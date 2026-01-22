64b32977f993772cd81efeff025da92ea41e963a
LoloRigolo <lolocritiqueurs21@gmail.com>
2026-01-22 16:00:39 +0100

    feat(frontend) : Title and scrollable chat

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,17 @@
 import ChatPage from "@/frontend/components/ChatPage";
 
 export default function Page() {
-  return <ChatPage />;
+  return (
+    <div className="h-dvh flex flex-col">
+      <div className="p-4">
+        <h1 className="text-5xl font-extrabold text-center mb-6 tracking-wide">
+          Bienvenue sur GOJO GPT
+        </h1>
+        <h2 className="text-xl text-center mb-6">Ryōiki Tenkai</h2>
+      </div>
+      <div className="flex-1 min-h-0">
+        <ChatPage />
+      </div>
+    </div>
+  );
 }
