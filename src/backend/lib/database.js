d86b44e7257e1ca88dc06b7571614beabc652f4f
LoloRigolo <lolocritiqueurs21@gmail.com>
2026-01-22 22:07:58 +0100

    fix(prisma): upgrading to v7.3 and deps

--- src/backend/lib/database.js
+++ src/backend/lib/database.js
@@ -1,12 +1,17 @@
 import { PrismaClient } from "@prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 let prisma;
 
 if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
+  prisma = new PrismaClient({
+    accelerateUrl: process.env.DATABASE_URL,
+  }).$extends(withAccelerate());
 } else {
   if (!global.prisma) {
-    global.prisma = new PrismaClient();
+    global.prisma = new PrismaClient({
+      accelerateUrl: process.env.DATABASE_URL,
+    }).$extends(withAccelerate());
   }
   prisma = global.prisma;
 }
